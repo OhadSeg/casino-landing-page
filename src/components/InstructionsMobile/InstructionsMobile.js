2a26adc72de658804f9a85d79cd0b956252b5b39
Ohad Segal <segalohad@gmail.com>
2023-03-27 10:17:21 +0300

    css changes, adding carousel component

--- src/components/InstructionsMobile/InstructionsMobile.js
+++ src/components/InstructionsMobile/InstructionsMobile.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import './Instructions.css';
+import './InstructionsMobile.css';
 import Icon1 from '../../assets/Icons/number-one.png';
 import Icon2 from '../../assets/Icons/number-two.png';
 import Icon3 from '../../assets/Icons/number-three.png';
 import InstructionItem from '../InstructionItem/InstructionItem';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 520, min: 0 },
+    items: 1
+  }
+};
 
 const instructionList = [
   {
@@ -28,20 +50,20 @@ const instructionList = [
 
 function Instructions() {
   return (
-    <div className="instructions-container">
+    <Carousel responsive={responsive} className="instructions-container">
       {instructionList.map((instruction, index) => {
         return (
-          <>
+          <div className="instruction-container">
             <InstructionItem data={instruction}></InstructionItem>
-            {index !== instructionList.length - 1 && (
+            {(index !== instructionList.length - 1) && (
               <p>
                 <i class="arrow right"></i>
               </p>
             )}
-          </>
+          </div>
         );
       })}
-    </div>
+    </Carousel>
   );
 }
 
--- src/components/Middle/Middle.js
+++ src/components/Middle/Middle.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GamesList from '../GamesList/GamesList';
 import Instructions from '../Instructions/Instructions';
+import InstructionsMobile from '../InstructionsMobile/InstructionsMobile';
 import MainHeader from '../MainHeader/MainHeader';
 import './Middle.css';
 
@@ -12,9 +13,26 @@ const Middle = () => {
       <div style={{padding:'0rem 6rem '}}>
         <hr />
       </div>
-      <Instructions />
+      { window.innerWidth > 500 ? (
+              <Instructions />
+          ) : (
+            <InstructionsMobile/>
+          )}    
     </div>
   );
 };
 
 export default Middle;
+
+// const Middle = () => {
+//   return (
+//     <div className="middle-part">
+//       <MainHeader />
+//       <GamesList />
+//       <div style={{padding:'0rem 6rem '}}>
+//         <hr />
+//       </div>
+//       <Instructions />
+//     </div>
+//   );
+// };
